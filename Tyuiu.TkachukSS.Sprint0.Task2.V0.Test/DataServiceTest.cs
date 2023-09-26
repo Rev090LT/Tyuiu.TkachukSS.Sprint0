ebe72bc363debed544b15945b31f12fde240cf57
Rev090LT <mamontosaimon900@gmail.com>
2023-09-26 22:20:15 +0500

    Task2

---
+++ Tyuiu.TkachukSS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TkachukSS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.TkachukSS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataSerivceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Семён";
+            var res = DataSerivce.GetMessage(name);
+
+            Assert.AreEqual("Привет, Семён", res);
+        }
+    }
+}
---
+++ Tyuiu.TkachukSS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tyuiu.TkachukSS.Sprint0.Task2.V0.Lib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TkachukSS.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataSerivce.GetMessage("Семён"));
+            Console.ReadKey();          
+        }
+    }
+}
