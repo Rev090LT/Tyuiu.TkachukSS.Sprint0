d4320f9aa99baa9a2595290c31c2aca9f67e8a90
Rev090LT <mamontosaimon900@gmail.com>
2023-09-28 00:31:10 +0500

    Task3

---
+++ Tyuiu.TkachukSS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TkachukSS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.TkachukSS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.summ(5, 5));
+        }
+    }
+}
